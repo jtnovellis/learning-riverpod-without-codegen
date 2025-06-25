b4267e7482fcf2c1621f2875385977441d7c5ef4
Sillevon <sillevonsas@gmail.com>
2025-06-25 18:38:10 -0500

    family future provider

--- lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/06_family_future_provider/future_provider_screen.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class FamilyFutureScreen extends StatelessWidget {
+class FamilyFutureScreen extends ConsumerStatefulWidget {
   const FamilyFutureScreen({super.key});
 
   @override
+  ConsumerState<FamilyFutureScreen> createState() => _FamilyFutureScreenState();
+}
+
+class _FamilyFutureScreenState extends ConsumerState<FamilyFutureScreen> {
+  int pokemonId = 1;
+
+  @override
   Widget build(BuildContext context) {
+    final pokemonAsync = ref.watch(pokemonProvider(pokemonId));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Family Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (name) => Text(name),
+          error: (error, _) => Text(error.toString()),
+          loading: () => const CircularProgressIndicator(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+        child: const Icon(Icons.refresh),
+        onPressed: () {
+          setState(() {
+            pokemonId += 1;
+          });
+        },
       ),
     );
   }
