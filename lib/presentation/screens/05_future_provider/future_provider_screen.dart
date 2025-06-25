04d964bc16edcc804eef698748972a06ef06b6e9
Sillevon <sillevonsas@gmail.com>
2025-06-25 18:32:03 -0500

    future provider

--- lib/presentation/screens/05_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/05_future_provider/future_provider_screen.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
-
-class FutureProviderScreen extends StatelessWidget {
+class FutureProviderScreen extends ConsumerWidget {
   const FutureProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonAsync = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonAsync.when(
+          data: (name) => Text(name),
+          error: (error, _) => Text(error.toString()),
+          loading: () => const CircularProgressIndicator(),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+        child: const Icon(Icons.refresh),
+        onPressed: () {
+          ref.read(pokemonIdProvider.notifier).update((state) => state + 1);
+        },
       ),
     );
   }
