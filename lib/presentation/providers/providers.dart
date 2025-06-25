13c415b93c149f2ca173af5551eb8976a9a58a59
Sillevon <sillevonsas@gmail.com>
2025-06-25 18:47:36 -0500

    stream provider

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,3 +2,4 @@ export 'simple_name_provider.dart';
 export 'state_providers.dart';
 export 'todo_providers.dart';
 export 'future_providers.dart';
+export 'stream_provider.dart';
--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -9,6 +10,31 @@ class StreamProviderScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stream Provider'),
+      ),
+      body: const _StreamView(),
+    );
+  }
+}
+
+class _StreamView extends ConsumerWidget {
+  const _StreamView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final usersInChat = ref.watch(usersInChatProvider);
+
+    return Container(
+      child: usersInChat.when(
+        data: (users) => ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(users[index]),
+            );
+          },
+        ),
+        error: (error, _) => Text(error.toString()),
+        loading: () => const CircularProgressIndicator(),
       ),
     );
   }
